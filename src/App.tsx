4cae05bb98ae50feefd03edf785d0b23fa74be5a
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2023-09-20 19:58:16 +0100

    removed unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
 import React, {useEffect, useState} from 'react';
-// import logo from './logo.svg';
 import './App.css';
-// import FaceSelector, {SelectableChris} from './components/FaceSelector';
 import PollSelector from './components/PollSelector';
 import { fetchWikipediaPolls } from './lib/cheerio';
 import { ScrappedPoll } from "./lib/ScrappedPoll";
 import SainteLagueResultsTable from './components/SainteLague';
 
-// let chrises = Array<SelectableChris>();
-// chrises.push({key: "chris-right", alt: "face of Chris Luxon", image: "chris-right.png"});
-// chrises.push({key: "chris-left", alt: "face of Chris Hipkins", image: "chris-left.png"});
-
 function App() {
-  // const [selectedChris, setSelectedChris] = useState('chris-right');
   const [polls, setPolls] = useState<ScrappedPoll[]>([])
   const [selectedPoll, setSelectedPoll] = useState<ScrappedPoll | null>(null);
   const [maoriElectorateSeats, setMaoriElectorateSeats] = useState<number>(3);
@@ -34,7 +27,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Select poll and see potential coallition combinations
         </p>
@@ -46,7 +38,6 @@ function App() {
         >
           Source Code
         </a>
-        {/* <FaceSelector selectedChris={selectedChris} setSelectedChris={setSelectedChris} chrises={chrises}/> */}
       </header>
       <main>
       <PollSelector polls={polls} maoriElectorateSeats={maoriElectorateSeats} setMaoriElectorateSeats={setMaoriElectorateSeatsHandler} setSelectedPoll={setSelectedPollHandler} selectedPoll={selectedPoll}/>
